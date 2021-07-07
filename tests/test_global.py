06b74aadee47cc94f440387e84e2e1f3474bb997
João Pedro Chagas <jpchagas@gmail.com>
2021-07-06 23:59:23 -0300

    fix pytest issues

---
+++ tests/test_global.py
@@ -0,0 +1,10 @@
+class GlobalPrice:
+
+    def test_insert_prices(self):
+        assert True
+
+    def test_get_prices(self):
+        assert True
+
+    def test_get_price(self):
+        assert True
--- xepacrawler/xepacrawler/test_url_generator.py
+++
@@ -1,5 +0,0 @@
-from xepacrawler.xepacrawler.utils.url_helper import UrlHelper
-
-u = UrlHelper()
-
-u.range_urls("01/01/2021", "07/05/2021")
