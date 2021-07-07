9f80418a1a8e1d21fdf00e2cf3fc5c6ac9e40d9b
João Pedro Chagas <jpchagas@gmail.com>
2021-07-07 08:58:36 -0300

    fix endpoint to execute crawl

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
 import os
 
 app = Flask(__name__)
 
-sched = BackgroundScheduler(daemon=True)
 
-count = 0
-
-
-def run_spider():
-    sched.print_jobs()
+@app.route('/')
+def hello():
     os.chdir(os.getcwd() + "/xepacrawler")
     os.system('scrapy crawl ceasars')
+    return 'Hello, World!'
 
-
-sched.add_job(run_spider, 'cron', day='*')
-sched.start()
 
 if __name__ == "__main__":
     app.run()
